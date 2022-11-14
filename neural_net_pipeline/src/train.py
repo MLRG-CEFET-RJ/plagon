36831ecb5acf85d0ab0ec5e32cd301f48ea8d534
Jorge Gabriel Rodrigues <jorgegabriel99h@gmail.com>
2022-11-14 12:08:59 -0300

    #refs: test subdir

--- neural_net_pipeline/src/train.py
+++ neural_net_pipeline/src/train.py
@@ -161,7 +161,9 @@ def plot_losses(history):
 
 def get_partition(datasets_dir, subdir):
     labels_filename = os.path.join(datasets_dir, subdir, 'labels.npy')
+    print(subdir)
     subdir = os.path.join(datasets_dir, subdir)
+    print(subdir)
     filelist = os.listdir(subdir)
     filelist.sort()
     filelist.pop() # remove the last item (i.e., file name 'labels.npy')
